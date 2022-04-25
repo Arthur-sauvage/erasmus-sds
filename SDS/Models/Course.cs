46363f7b5bd7302ab944fe6eed4d086be37faa91
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-04-25 14:54:14 +0200

    add attributes (speciality and Ects)

--- SDS/Models/Course.cs
+++ SDS/Models/Course.cs
@@ -7,6 +7,8 @@ namespace SDS.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public string? Speciality { get; set; }
+        public int Ects { get; set; }
         public int Likes { get; set; }
         public int Difficulty { get; set; }
     }
