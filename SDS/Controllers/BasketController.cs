21aa2234e3efd36f8d242dffb22d3b7735fb4a9e
Ignistes <alex.mace45@gmail.com>
2022-06-13 15:42:00 +0200

    add pdf generation

--- SDS/Controllers/BasketController.cs
+++ SDS/Controllers/BasketController.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SDS.Data;
 using SDS.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+using Syncfusion.Pdf.Grid;
 
 namespace SDS.Controllers
 {
@@ -25,27 +30,7 @@ namespace SDS.Controllers
         // GET: Basket
         public async Task<IActionResult> Index()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            List<Basket> listBasket = _context.Basket.ToList();
-            List<String> listIdCourse = new List<String>();
-            foreach(Basket b in listBasket){
-                if(string.Equals(b.idStudent,userId))
-                {
-                    listIdCourse.Add(b.idCourse);
-                }
-            }
-            var listCourse = _context.Course.ToList();
-            var courseBasket = new List<Course>();
-            foreach(String courseID in listIdCourse)
-            {
-                foreach (var c in listCourse)
-                {
-                    if(c.Id == Int32.Parse(courseID))
-                    {
-                        courseBasket.Add(c);
-                    }
-                }
-            }
+            var courseBasket = getBasketCourse();
             return View(courseBasket);
         }
 
@@ -91,7 +76,7 @@ namespace SDS.Controllers
             }
 
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var basket = await _context.Basket.FirstOrDefaultAsync(m => string.Equals(m.idBasket,userId+id));
+            var basket = await _context.Basket.FirstOrDefaultAsync(m => string.Equals(m.idBasket, userId + id));
             if (basket == null)
             {
                 return NotFound();
@@ -100,5 +85,87 @@ namespace SDS.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> CreatePDF(string id)
+        {
+
+            var courseBasket = getBasketCourse();
+
+
+
+            //Generate a new PDF document.
+            PdfDocument doc = new PdfDocument();
+            //Add a page.
+            PdfPage page = doc.Pages.Add();
+            //Create a PdfGrid.
+            PdfGrid pdfGrid = new PdfGrid();
+            PdfGraphics graphics = page.Graphics;
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+            //Add values to list
+            //Draw the text
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.Users.FindAsync(userId);
+            graphics.DrawString(user.FirstName + " " + user.LastName, font, PdfBrushes.Black, new PointF(0, 0));
+
+            List<object> data = new List<object>();
+
+            //Object row;
+            for (int i = 0; i < courseBasket.Count; i++)
+            {
+                var course = courseBasket[i];
+                var row = new { Name = course.Name, Speciality = course.Speciality, ECTS = course.Ects };
+                data.Add(row);
+            }
+
+            
+            //Add list to IEnumerable
+            IEnumerable<object> dataTable = data;
+            //Assign data source.
+            pdfGrid.DataSource = dataTable;
+            //Draw grid to the page of PDF document.
+            pdfGrid.Draw(page, new Syncfusion.Drawing.PointF(10, 50));
+            //Write the PDF document to stream
+            MemoryStream stream = new MemoryStream();
+            doc.Save(stream);
+            //If the position is not set to '0' then the PDF will be empty.
+            stream.Position = 0;
+            //Close the document.
+            doc.Close(true);
+            //Defining the ContentType for pdf file.
+            string contentType = "application/pdf";
+            //Define the file name.
+            string fileName = "Learning_agreement_" + user.FirstName +"_"+ user.LastName + ".pdf";
+            //Creates a FileContentResult object by using the file contents, content type, and file name.
+            return File(stream, contentType, fileName);
+        }
+
+        private List<Course> getBasketCourse()
+        {
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            List<Basket> listBasket = _context.Basket.ToList();
+            List<String> listIdCourse = new List<String>();
+            foreach (Basket b in listBasket)
+            {
+                if (string.Equals(b.idStudent, userId))
+                {
+                    listIdCourse.Add(b.idCourse);
+                }
+            }
+            var listCourse = _context.Course.ToList();
+            var courseBasket = new List<Course>();
+            foreach (String courseID in listIdCourse)
+            {
+                foreach (var c in listCourse)
+                {
+                    if (c.Id == Int32.Parse(courseID))
+                    {
+                        courseBasket.Add(c);
+                    }
+                }
+            }
+            return courseBasket;
+        }
+
     }
 }
